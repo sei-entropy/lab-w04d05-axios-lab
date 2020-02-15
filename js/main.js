2f8f3192ecc5d437df30ae67923b10f73d1c06b6
AbdulhamidAlelaiyan <quickisslow191@gmail.com>
2020-02-15 18:28:35 +0300

    Added HTML Content and CSS Styling

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// document.querySelector('#year-input').;
+//
+//
+// const requestObject = {
+//    method: 'get',
+//    url: `https://calendarific.com/api/v2/holidays?api_key=1d72dde247729e6fb0d4a0221298a540e8ecdea8&year=${year}&country=SA`,
+// };
